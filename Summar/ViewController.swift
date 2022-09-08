674bbe679ee339c60de3f6077c52b82d3486074e
SeungUkShin <krdut1@gmail.com>
2022-09-08 21:46:58 +0900

    Initial Commit

---
+++ Summar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Summar
+//
+//  Created by ukBook on 2022/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
